217c6a99828e0e418d3dabea140e8c142693a894
Faizul Osman <faizulosmanrabby@gmail.com>
2022-12-10 23:11:57 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-
-
 export default function Home() {
-	return (
-		<div>
-			<h1>This is Saurav Mondal</h1>
-			<h1>This is Samrat Joydhar</h1>
-			<h1>This is Shohug Mahbub</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>This is Saurav Mondal</h1>
+      <h1>This is Samrat Joydhar</h1>
+      <h1>This is Shohug Mahbub</h1>
+      <h1>This is Faizul Osman</h1>
+    </div>
+  );
 }
