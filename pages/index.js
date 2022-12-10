5680299ebe5bd6cd8f1f9f5d1256ae4889cb8a0f
joydharsamrat <joydharsamrat@gmail.com>
2022-12-10 16:09:54 +0600

    testing Samrat Joydhar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<h1>This is Saurav Mondal</h1>
+			<h1>This is Samrat Joydhar</h1>
 		</div>
 	);
 }
