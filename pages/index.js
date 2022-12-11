a8719e3870e91b7552cad81069d484e65eb8456e
Shakhawat2 <shohugmahbub@gmail.com>
2022-12-11 15:25:23 +0600

    changed background

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Banner from "../Components/Banner/banner";
 import CharityDifference from "../Components/Charity_With_Difference/CharityDifference";
 import Contribute from "../Components/Contribute/Contribute";
 
@@ -13,6 +14,9 @@ export default function Home() {
 	}, [])
 	return (
 		<div>
+			<Banner></Banner>
+			<CharityDifference></CharityDifference>
+			<Contribute></Contribute>
 			<h1>This is Saurav Mondal</h1>
 			<h1>This is Samrat Joydhar</h1>
 			<h1>This is Shohug Mahbub</h1>
