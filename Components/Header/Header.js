7d49a3992e9323e7acba4172f7fe029fc2987460
indrajith76 <indrajithgoswami@gmail.com>
2022-12-12 02:13:59 +0600

    added update nav

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -5,7 +5,7 @@ import {
   BsCheckCircleFill,
   BsFacebook,
   BsInstagram,
-  BsLinkedin, 
+  BsLinkedin,
   BsTelephoneFill,
   BsTwitter,
 } from "react-icons/bs";
@@ -44,8 +44,8 @@ const Header = () => {
     </>
   );
   return (
-    <div className="sticky top-0 z-40 bg-[#ffffffd7] ">
-      <div className="bg-slate-700 hidden lg:block">
+    <>
+      <div className="bg-slate-700 hidden lg:block sticky -top-0">
         <div className="flex flex-col lg:flex-row lg:justify-between container mx-auto py-3 px-5">
           <div className="flex justify-center items-center gap-5">
             <Link className="font-bold text-lg" href="">
@@ -85,86 +85,88 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div class="relative flex items-center justify-between">
-          <a
-            href="/"
-            aria-label="Company"
-            title="Company"
-            class="inline-flex items-center"
-          >
-            <Image
-              src="/logo/header-logo.png"
-              alt="Picture of the author"
-              width={250}
-              height={250}
-            />
-          </a>
-          <ul class="flex items-center hidden space-x-8 lg:flex text-slate-800 font-semibold">
-            {navitems}
-          </ul>
-          <div class="lg:hidden">
-            <button
-              aria-label="Open Menu"
-              title="Open Menu"
-              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
-              onClick={() => setIsMenuOpen(true)}
+      <div className="sticky top-0 z-40 bg-white">
+        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div class="relative flex items-center justify-between">
+            <a
+              href="/"
+              aria-label="Company"
+              title="Company"
+              class="inline-flex items-center"
             >
-              <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                />
-              </svg>
-            </button>
-            {isMenuOpen && (
-              <div class="absolute top-0 left-0 w-full z-30">
-                <div class="p-5 bg-white border rounded shadow-sm">
-                  <div class="flex items-center justify-between mb-4">
-                    <div>
-                      <Link href="/">
-                        <Image
-                          src="/logo/header-logo.png"
-                          alt="Picture of the author"
-                          width={150}
-                          height={150}
-                        ></Image>
-                      </Link>
+              <Image
+                src="/logo/header-logo.png"
+                alt="Picture of the author"
+                width={250}
+                height={250}
+              />
+            </a>
+            <ul class="flex items-center hidden space-x-8 lg:flex text-slate-800 font-semibold">
+              {navitems}
+            </ul>
+            <div class="lg:hidden">
+              <button
+                aria-label="Open Menu"
+                title="Open Menu"
+                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                onClick={() => setIsMenuOpen(true)}
+              >
+                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                  />
+                </svg>
+              </button>
+              {isMenuOpen && (
+                <div class="absolute top-0 left-0 w-full z-30">
+                  <div class="p-5 bg-white border rounded shadow-sm">
+                    <div class="flex items-center justify-between mb-4">
+                      <div>
+                        <Link href="/">
+                          <Image
+                            src="/logo/header-logo.png"
+                            alt="Picture of the author"
+                            width={150}
+                            height={150}
+                          ></Image>
+                        </Link>
+                      </div>
+                      <div>
+                        <button
+                          aria-label="Close Menu"
+                          title="Close Menu"
+                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <path
+                              fill="currentColor"
+                              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
                     </div>
-                    <div>
-                      <button
-                        aria-label="Close Menu"
-                        title="Close Menu"
-                        class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        onClick={() => setIsMenuOpen(false)}
-                      >
-                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-                          <path
-                            fill="currentColor"
-                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
+                    <nav className="text-slate-800">
+                      <ul class="space-y-4">{navitems}</ul>
+                    </nav>
                   </div>
-                  <nav className="text-slate-800">
-                    <ul class="space-y-4">{navitems}</ul>
-                  </nav>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
