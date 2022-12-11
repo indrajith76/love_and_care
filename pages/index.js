c70b2b807033a575813b7115c1d0c6d0b1b536ac
Shakhawat2 <shohugmahbub@gmail.com>
2022-12-11 09:23:45 +0600

    charity_with_difference section created

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Banner from "../Components/Banner/banner";
+import CharityDifference from "../Components/Charity_With_Difference/CharityDifference";
 
 
 export default function Home() {
 	return (
 		<div>
+			<CharityDifference></CharityDifference>
 			<h1>This is Saurav Mondal</h1>
 			<h1>This is Samrat Joydhar</h1>
 			<h1>This is Shohug Mahbub</h1>
