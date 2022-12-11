916142b2f7a3c98a085eafde81b482fff4c68cea
indrajith76 <indrajithgoswami@gmail.com>
2022-12-12 02:07:16 +0600

    added style

--- Components/GetInspire/getInspire.js
+++ Components/GetInspire/getInspire.js
@@ -7,9 +7,9 @@ import features4 from '../../public/getInspire/thm-shape-12.png';
 
 const getInspire = () => {
 	return (
-		<div className="text-center py-12 m-auto">
+		<div className="text-center py-12 m-auto container">
 			<div className="grid md:grid-cols-3 sm:grid-cols-1 ">
-				<figure className="relative ">
+				<figure className="relative overflow-hidden">
 					<Image
 						className="cardImage h-full w-full duration-500 transform hover:scale-125"
 						src={features1}
@@ -17,7 +17,7 @@ const getInspire = () => {
 						height={400}
 						alt=""
 					></Image>
-					<div className="absolute  sm:left-20 top-0 text-center px-6 py-3 block">
+					<div className="absolute  sm:left-7 top-0 text-center px-6 py-3 block z-20">
 						<h2 className="card-title text-4xl pt-24 font-bold py-3 text-white">
 							We Fight Illiteracy
 						</h2>
@@ -26,7 +26,7 @@ const getInspire = () => {
 						</h2>
 					</div>
 
-					<div className="absolute flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0 transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70"></div>
+					<div className="absolute flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0 transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70 z-10"></div>
 				</figure>
 
 				<figure className="relative">
@@ -37,7 +37,7 @@ const getInspire = () => {
 						height={400}
 						alt=""
 					></Image>
-					<div className="absolute  sm:left-20 top-0 text-center px-6 py-3 block">
+					<div className="absolute  sm:left-7 top-0 text-center px-6 py-3 block z-20">
 						<h2 className="card-title text-4xl pt-24 font-bold text-white">
 							We Fight Illiteracy
 						</h2>
@@ -46,7 +46,7 @@ const getInspire = () => {
 						</h2>
 					</div>
 
-					<div className="absolute flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0 transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70"></div>
+					<div className="absolute flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0 transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70 z-10"></div>
 				</figure>
 
 				<figure className="relative ">
@@ -57,7 +57,7 @@ const getInspire = () => {
 						height={400}
 						alt=""
 					></Image>
-					<div className="absolute sm:left-20 top-0 text-center px-6 py-3 block">
+					<div className="absolute sm:left-7 top-0 text-center px-6 py-3 block z-20">
 						<h2 className="card-title  text-4xl pt-24 font-bold text-white">
 							We Fight Illiteracy
 						</h2>
@@ -66,7 +66,7 @@ const getInspire = () => {
 						</h2>
 					</div>
 
-					<div className="absolute  flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0  transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70"></div>
+					<div className="absolute  flex items-center justify-center top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden opacity-0  transition duration-300 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-70 z-10"></div>
 				</figure>
 			</div>
 		</div>
