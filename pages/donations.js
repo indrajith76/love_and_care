ab25ce62d32ccf7740de7f40c8318cd78bb55201
indrajith76 <indrajithgoswami@gmail.com>
2022-12-14 00:32:24 +0600

    updated bug

--- pages/donations.js
+++ pages/donations.js
@@ -1,21 +1,26 @@
+import Head from "next/head";
 import Donations from "../Components/Donations/Donations";
 import { Footer } from "../Components/Footer";
 import Header from "../Components/Header/Header";
 
 const donations = () => {
-    return (
-        <>
-            <Header></Header>
-            <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 my-28">
-                    <Donations></Donations>
-                    <Donations></Donations>
-                    <Donations></Donations>
-                    <Donations></Donations>
-                    <Donations></Donations>                  
-            </div>
-            <Footer></Footer>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/logo/favicon.png" />
+        <title>Donations | Love&Care</title>
+      </Head>
+      <Header></Header>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 my-28">
+        <Donations></Donations>
+        <Donations></Donations>
+        <Donations></Donations>
+        <Donations></Donations>
+        <Donations></Donations>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default donations;
--- pages/event.js
+++ pages/event.js
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import React from 'react';
 import Challenge from '../Components/Event/Challenge/challenge';
 import Programme from '../Components/Event/Programme/programme';
 import WhyDonate from '../Components/Event/WhyDonate/whyDonate';
+import { Footer } from '../Components/Footer';
 import Header from '../Components/Header/Header';
 
 const Event = () => {
     return (
         <>
+        <Head>
+        <link rel="shortcut icon" href="/logo/favicon.png" />
+        <title>Events | Love&Care</title>
+      </Head>
         <Header/>
         <div className='lg:grid grid-cols-[2fr,1fr]'>
             <div className='py-20 px-5 lg:pr-10 lg:pl-24'>
@@ -18,6 +24,7 @@ const Event = () => {
                 <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam nemo cumque consectetur dolorem omnis, ex quidem adipisci recusandae nihil quo accusantium saepe eos aliquam facilis beatae rerum qui. Provident?</div>
             </div>
         </div>
+        <Footer/>
         </>
     );
 };
