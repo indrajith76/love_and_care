deabf690ad335fc4f246ad93702893745d5ef2b2
indrajith76 <indrajithgoswami@gmail.com>
2022-12-14 00:25:13 +0600

    updated bug

--- pages/event.js
+++ pages/event.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Challenge from '../Components/Event/Challenge/challenge';
 import Programme from '../Components/Event/Programme/programme';
 import WhyDonate from '../Components/Event/WhyDonate/whyDonate';
+import Header from '../Components/Header/Header';
 
 const Event = () => {
     return (
+        <>
+        <Header/>
         <div className='lg:grid grid-cols-[2fr,1fr]'>
             <div className='py-20 px-5 lg:pr-10 lg:pl-24'>
                 <Programme></Programme>
@@ -15,6 +18,7 @@ const Event = () => {
                 <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam nemo cumque consectetur dolorem omnis, ex quidem adipisci recusandae nihil quo accusantium saepe eos aliquam facilis beatae rerum qui. Provident?</div>
             </div>
         </div>
+        </>
     );
 };
 
